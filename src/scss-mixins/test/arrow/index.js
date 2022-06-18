0de386fdd5abb8921b3702126689974a63cab26d
zhaoliangshun <zhaoliangshun@jd.com>
2022-06-18 16:40:39 +0800

    arrow

---
+++ src/scss-mixins/test/arrow/index.js
@@ -0,0 +1,11 @@
+import './index.scss'
+export default () => {
+    return (
+        <>
+            <div className="top" />
+            <div className="bottom" />
+            <div className="left" />
+            <div className="right" />
+        </>
+    );
+};
