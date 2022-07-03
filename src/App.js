edd7febb60dfa0a352b5f34db3c460c4a28f01e8
zhaoliangshun <zhaoliangshun@jd.com>
2022-07-03 13:32:43 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import { useIDBKeyval } from './hooks';
+import { useCookie } from './hooks';
+import isPlainObject from 'lodash/isPlainObject';
 
+const aa = {
+    a: 1,
+    b: 2,
+    c: {
+        d: 3,
+    },
+    e: [4],
+};
+
+console.log(isPlainObject(aa));
+// '{"a":1,"b":2,"c":{"d":3},"e":[4]}'.replaceAll('"', '')
+// '{AAA:1,b:2,c:{d:3},e:[4]}'.replace(/([a-z]+):/gi,'"$1":')
 const App = () => {
-    const [value, setValue] = useIDBKeyval('aaa');
+    const [value, setValue] = useCookie('aaa', '*$#|');
     console.log(value);
-    return <div onClick={() => setValue('bbb', { expires: 365 })}>111</div>;
+    return <div onClick={() => setValue('*$#|[]', { expires: 365 })}>111</div>;
 };
 
 export default App;
