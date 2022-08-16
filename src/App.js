9f03dc5ca0af35b83517049d8d709bebb911f1a5
zhaoliangshun <zhaoliangshun@jd.com>
2022-08-16 17:57:31 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.scss';
+import Slider from './react-slick';
 
-const App = () => {
-    const inputRef = useRef();
-    const placeholderRef = useRef();
-    const [value, setValue] = useState();
-    const [isFocus, setIsFocus] = useState();
-    const onPlaceHolderClick = () => {
-        setIsFocus(true);
-        inputRef.current.focus();
-    };
-    const onInputBlur = () => {
-        setIsFocus(false);
-    };
-    return (
-        <div className="content">
-            <input
-                ref={inputRef}
-                className="input"
-                type="text"
-                value={value}
-                onChange={(e) => {
-                    setValue(e.target.value);
-                }}
-                onBlur={onInputBlur}
-            />
-            {!isFocus && !value ? (
-                <div ref={placeholderRef} onClick={onPlaceHolderClick} className="placeholder">
-                    aaa aaa aaa aaaaaa aaaaaa aaaaaa aaaaaa aaa
-                </div>
-            ) : (
-                ''
-            )}
-        </div>
-    );
+const baseUrl = 'https://s3.amazonaws.com/static.neostack.com/img/react-slick';
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+export default () => {
+  const SliderContentRef = useRef();
+  const [settings, setSettings] = useState();
+  const [isload, setIsload] = useState(false);
+  useEffect(() => {
+    setIsload(true);
+    if (SliderContentRef.current.offsetWidth < arr.length * 100) { //fa
+      setSettings({
+        dots: false,
+        infinite: false,
+        speed: 100,
+        initialSlide: 0,
+        swipeToSlide: true,
+        variableWidth: true,
+        arrows: true,
+        slidesToScroll: Math.floor(SliderContentRef.current.offsetWidth / 100),
+        slidesToShow: Math.floor(SliderContentRef.current.offsetWidth / 100),
+      });
+    } else {
+      setSettings('unslick');
+    }
+  }, []);
+  return (
+    <div ref={SliderContentRef} style={{ width: '80%', margin: 'auto' }}>
+      {isload &&
+        (settings === 'unslick' ? (
+          arr.map((item, index) => {
+            return (
+              <h1 style={{ width: 100 }} key={index}>
+                {item}
+              </h1>
+            );
+          })
+        ) : (
+          <Slider {...settings}>
+            {arr.map((item, index) => {
+              return (
+                <h1 style={{ width: 100 }} key={index}>
+                  {item}
+                </h1>
+              );
+            })}
+          </Slider>
+        ))}
+    </div>
+  );
 };
-
-export default App;
